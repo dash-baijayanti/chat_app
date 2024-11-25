df59c3e7aaaf414d259136a0565740fee26ab5e5
dash-baijayanti <dash.baijayanti@gmail.com>
2024-11-25 22:32:52 +0100

    5.1

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { View, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 
 const Chat = ({ route }) => {
@@ -15,31 +15,54 @@ const Chat = ({ route }) => {
     setMessages([
       {
         _id: 1,
-        text: "Hello developer",
+        text: 'Hello developer',
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
         },
+      },
+      {
+        _id: 2,
+        text: 'This is a systemmessage',
+        createdAt: new Date(),
+        system: true,
       },
     ]);
   }, []);
+  
   const onSend = (newMessages) => {
     setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
   }
+
+  const renderBubble = (props) => {
+    return ( 
+      <Bubble 
+        {...props} 
+        textStyle={{ 
+              right: { color: 'white' }, 
+              left:{ color: 'black'}
+        }} 
+        wrapperStyle={{
+            right: { backgroundColor: 'black', }, 
+            left: { backgroundColor: 'white', }, 
+        }} 
+      />
+    )}
 
   return (
     <View style={[styles.container, { backgroundColor }]}>
      <GiftedChat
       messages={messages}
+      renderBubble={renderBubble}
       onSend={messages => onSend(messages)}
       user={{
         _id: 1
       }}
     />
       {Platform.OS === 'android' ? 
-      <KeyboardAvoidingView behavior="height" /> : null 
+      <KeyboardAvoidingView behavior='height' /> : null 
       }
     </View>
   );
@@ -48,13 +71,6 @@ const Chat = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcomeText: {
-    fontSize: 24,
-    color: '#333',
-    fontWeight: 'bold',
   },
 });
 
